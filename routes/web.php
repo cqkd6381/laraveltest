7bf5e11c4fb9229db0f3ca6c09613b001da86063
cqkd6381 <cqkd6381@163.com>
2016-12-13 15:41:42 +0800

    modified routes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,12 @@ Route::group(['prefix' => 'user'], function () {
 	Route::any('store','UserController@store')->name('store');
 	Route::any('delete/{id}','UserController@delete')->name('delete');
 });
+
+Route::get('api/users/{user}', function (App\User $user) {
+//    return $user->email;
+    dd($user);
+});
+Route::any('test/{id}',function($id){
+    header('Access-Control-Allow-Origin: *');
+    return $id;
+});
