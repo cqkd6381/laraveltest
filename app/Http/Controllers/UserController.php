2b0da4ef95d4e92804a0c0b0193a7cecc4369943
cqkd6381 <cqkd6381@163.com>
2016-12-26 16:24:05 +0800

    UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
-    //
 
     public function index()
     {
